ab83079481bb4f0fe87d17c33344d623fb81b0e4
Vishal Yadav <vishalyadavdeoria274001@gmail.com>
2022-06-24 10:48:09 +0530

    Adding new mobile responsiveness

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ng-video-games'`, () => {
+  it(`should have as title 'games'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ng-video-games');
+    expect(app.title).toEqual('games');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ng-video-games app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('games app is running!');
   });
 });
